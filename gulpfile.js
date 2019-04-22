750a2303bcf2a23c5504c625f4c5884632b8a127
Chris <cstultz@oswego.edu>
2019-04-22 10:00:13 -0500

    dude nyce

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,31 @@
 var gulp = require('gulp'),
-watch = require ('gulp-watch');
+watch = require ('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested');
 
 gulp.task('default', function() {
     console.log("hurray, you created a gulp task");
-
-})
+});
 
 gulp.task('html', function() {
     console.log("Imagine something useful being done to your html here.");
-})
+});
 
 gulp.task('styles', function() {
     console.log("Imagine sass or PostCSS tasks running here.");
+    return gulp.src('./app/assets/styles/styles.css')
+    .pipe(postcss([cssvars, nested, autoprefixer]))
+    .pipe(gulp.dest('./app/temp/styles'));
 });
 
 gulp.task('watch', function() {
-
     watch('./app/index.html', function() {
         gulp.start('html');
     });
 
-    watch('./app/assets/styles/**/styles.css', function() {
+    watch('./app/assets/styles/**/*.css', function() {
         gulp.start('styles');
     });
 });
