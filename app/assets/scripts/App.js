17d42bc33eae28dd5e071bfe2fe954008f133c0f
Chris <cstultz@oswego.edu>
2019-07-25 17:17:06 -0500

    shoenice

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,18 +1,3 @@
-var $ = require('jquery');
-/* var Person = require('./modules/Person.js'); old wai */
-import Person from './modules/Person';
+import MobileMenu from './modules/MobileMenu';
 
-class Adult extends Person {
-    payTaxes() {
-        console.log(this.name + " now owes $0 in taxes. ");
-    }
-}
-
-alert("slut puck poop ninny")
-
-var john = new Person("John Doe", "blue");
-john.greet();
-
-var jane = new Adult("Jane Smith", "ass");
-jane.greet();
-jane.payTaxes();
+var mobileMenu = new MobileMenu();
