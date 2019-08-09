2bb9af7ee709799fa1e2232c50370e5be182320c
Chris <cstultz@oswego.edu>
2019-08-09 16:03:34 -0500

    shoenice

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,6 +1,7 @@
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader.js';
+import Modal from  './modules/Modal';
 import $ from 'jquery';
 
 
@@ -9,4 +10,5 @@ var mobileMenu = new MobileMenu();
 new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonial"), "60%");
 var stickyHeader = new StickyHeader();
+var modal = new Modal();
 
